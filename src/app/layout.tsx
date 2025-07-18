9d55c70e0c70b72416da1ea8217ffe19719e9191
Shahab Mir <m.mirghorbani@gmail.com>
2025-07-18 13:45:28 -0700

    Font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,22 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const cormorantGaramond = localFont({
+  src: [
+    {
+      path: "../../public/font/Cormorant_Garamond/CormorantGaramond-VariableFont_wght.ttf",
+      weight: "300 700",
+      style: "normal",
+    },
+    {
+      path: "../../public/font/Cormorant_Garamond/CormorantGaramond-Italic-VariableFont_wght.ttf",
+      weight: "300 700",
+      style: "italic",
+    },
+  ],
+  variable: "--font-cormorant-garamond",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${cormorantGaramond.variable} font-serif antialiased`}
       >
         {children}
       </body>

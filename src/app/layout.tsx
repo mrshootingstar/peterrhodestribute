0b514352f67a5250f367da745601d7f10107f9c3
Shahab Mir <m.mirghorbani@gmail.com>
2025-07-19 19:37:47 -0700

    Candle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,15 @@ const cormorantGaramond = localFont({
 export const metadata: Metadata = {
   title: "Peter Frederick Rhodes Memorial - In Loving Memory",
   description: "A place to remember, share, and celebrate Peter's life. Share your memories and tributes to honor Peter Frederick Rhodes (1948-2025).",
+  icons: {
+    icon: [
+      {
+        url: '/candle.svg',
+        type: 'image/svg+xml',
+      }
+    ],
+    shortcut: '/candle.svg',
+  },
 };
 
 export default function RootLayout({
